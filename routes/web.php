78135d0131df005705d7ea4d061f21f37a8487e6
bkamagwork <70734473+bkamagwork@users.noreply.github.com>
2021-04-14 13:59:41 +0800

    crud

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\PostController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -17,6 +17,21 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::resource('/posts',PostController::class);
+
+// Route::middleware(['auth'])->group(function () {
+//     Route::resource('posts',PostController::class)->only([
+//         'create', 'edit', 'destroy', 'store', 'update'
+//     ]);
+// }); 
+
+// Route::get('/posts',[PostController::class,'index'])->name('index');
+// Route::get('/posts/{id}',[PostController::class,'show'])->name('show');
+
+// My Account: 
+//  email: dennis.paralejas10@gmail.com
+//  password: august092019
